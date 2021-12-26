0c1a238df1513f4186f2c76385166a1bc07b89e3
1542229460 <1542229460@qq.com>
2021-12-26 22:14:26 +0800

    初始化

--- src/main/java/com/example/testgit/controller/Test.java
+++ src/main/java/com/example/testgit/controller/Test.java
@@ -1,4 +1,12 @@
 package com.example.testgit.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("test")
 public class Test {
+    @GetMapping("index")
+    public String test(){
+        return "success";
+    }
 }
